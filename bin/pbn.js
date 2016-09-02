67e5bdf4d1df4d7e48bb640cd6bec56955af8471
Richard Schneider <makaretu@gmail.com>
2016-09-03 10:51:45 +1200

    feat(pbn): 'pbn' is a cli to transform a PBN file into a JSON
    pbn [pbn-file]
    
    if pbn-file is not specified, then stdin is used.

---
+++ bin/pbn.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+
+var pbn = require('../index'),
+    fs = require('fs'),
+    process = require('process');
+
+var doc = process.stdin;
+if (process.argv[2])
+    doc = fs.createReadStream(process.argv[2]);
+
+doc
+    .pipe(pbn())
+    .on('data', data => {
+        console.log(JSON.stringify(data));
+    })
+    .on('error', err => {
+        process.stderr.write(err.message);
+        process.exit(1);
+    });
